a1e5cfb48882fda15abbf498e5e5042f26b0f6ff
Annika <annika.schiefner@icloud.com>
2018-08-08 01:06:21 +0200

    Changes to randomizers and inputfiles: -Deleted spaces around commas and some long items in input files, -Added reorganisation of files for reading into open sesame experiment as lists.

--- Randomizer_NGTsigns.py
+++ Randomizer_NGTsigns.py
@@ -1,91 +1,134 @@
-###Randomizer für Annika
+###randomizer für annika
 from random import shuffle, choice, random, randint
 import sys
 
-#Read input file
+#read input file
 def readfile(infilename):
     inputfile = open(infilename, 'r')
     vidlist = []
     for line in inputfile:
-        line = line.replace('\n','')    #Replace newlines from original file by commas
-        line = line.split(',')          #Split lines by comma
-        vidlist.append(line)            #Create a list with all read items
+        line = line.replace('\n','')    #replace newlines from original file by commas
+        line = line.split(',')          #split lines by comma
+        vidlist.append(line)            #create a list with all read items
     return vidlist
 
-#Create output file
-def outputfile(outfilename, newlist):
+#create output file
+def outputfile(outfilename, newlist, header):
     outfile = open(outfilename, 'w')
-    outfile.write("Gloss, corr_resp, other_resp \n")    #Header line: Gloss of the sign, expected response, distractor
-    for i in range(len(newlist)):
-        outfile.write(newlist[i][0] + "," + newlist[i][1] + ',' + newlist[i][2] +  '\n')    #Write items in output file
+    outfile.write(header)
+    for z in range(len(newlist)):
+        outfile.write(newlist[z][0] + "," + newlist[z][1] + ',' + newlist[z][2] + ',' + newlist[z][3] + ',' + newlist[z][4] + ',' + newlist[z][5] + '\n')    #write items in output file
     outfile.close()
 
 
-inputlist = readfile("NGTsigns.txt")   #Inputfile: GLOSS, FMU, translation1, translation2,..., translationN
+inputlist = readfile("ngtsigns.txt")   #inputfile: gloss, fmu, translation1, translation2,..., translationn
 
 def randomiser(number, vidlist):
     for r in range(number):
-        #Create list of FMUs with translation options
-        translation = []  # List of translation options per item, to be stacked within list structure
-        templist = []  # Serves for matching FMUs with translation options
-        temptrans = []  # Serves for temporarily storing the current translation list
-        experimentlist = []  # List with stacked list per line and within that a stacked list with the translation
-        outputlist = []  # List with structure for outputfile
+        #create list of fmus with translation options
+        translation = []  # list of translation options per item, to be stacked within list structure
+        templist = []  # serves for matching fmus with translation options
+        temptrans = []  # serves for temporarily storing the current translation list
+        experimentlist = []  # list with stacked list per line and within that a stacked list with the translation
+        outputlist = []  # list with structure for outputfile
         for i in range(len(vidlist)):
             for j in range(len(vidlist[i])):
                 if j < 2:
-                    translation = []    #Separate GLOSS and FMU from translations
+                    translation = []    #separate gloss and fmu from translations
                 else:
-                    translation.append(vidlist[i][j])   #Append all translations in line
-            templist.append(vidlist[i][0])  #Append GLOSS to templist
-            templist.append(vidlist[i][1])  #Append FMU to templist
-            templist.append(translation)    #Append all translation options for FMU in this line
+                    translation.append(vidlist[i][j])   #append all translations in line
+            templist.append(vidlist[i][0])  #append gloss to templist
+            templist.append(vidlist[i][1])  #append fmu to templist
+            templist.append(translation)    #append all translation options for fmu in this line
             experimentlist.append(templist)
             templist = []
 
         for i in range(len(experimentlist)):
-            Gloss = experimentlist[i][0]
-            FMU = experimentlist[i][1]
+            gloss = experimentlist[i][0]
+            fmu = experimentlist[i][1]
             temptrans = experimentlist[i][2]
             #print(temptrans)
 
-            # Search for correct response -> shares FMU with GLOSS-line
+            # search for correct response -> shares fmu with gloss-line
             l = randint(0,len(experimentlist)-1)
-            while experimentlist[l][1] != FMU or l == i:
-                l = randint(0,len(experimentlist)-1)    #Search for a line that has the target FMU and is different from line i
-            o = randint(0,len(experimentlist[l][2])-1)  #Within line, pick a random translation
+            while experimentlist[l][1] != fmu or l == i:
+                l = randint(0,len(experimentlist)-1)    #search for a line that has the target fmu and is different from line i
+            o = randint(0,len(experimentlist[l][2])-1)  #within line, pick a random translation
             while experimentlist[l][2][o] in temptrans:
-                print(experimentlist[l][2][o])
-                print(temptrans)
+                print('i found my own translation')
                 o = randint(0,len(experimentlist[l][2])-1)
             corr_resp = experimentlist[l][2][o]
 
-            #Search for distractor -> FMU different from GLOSS-line
+            #search for distractor -> fmu different from gloss-line
             p = randint(0,len(experimentlist)-1)
-            if FMU == "WID" or FMU == "IND":
-                while experimentlist[p][1] == FMU or experimentlist[p][1] in ("WID","IND"): #IND and WID refer to the same form/unit, therefore distractors for one can't have an FMU from the other
-                    p = randint(0,len(experimentlist)-1)    #Search for a line that has an FMU different from that in GLOSS line
-                q = randint(0,len(experimentlist[p][2])-1)  #Within line, pick a random translation
+            if fmu == "wid" or fmu == "ind":
+                while experimentlist[p][1] == fmu or experimentlist[p][1] in ("wid","ind"): #ind and wid refer to the same form/unit, therefore distractors for one can't have an fmu from the other
+                    p = randint(0,len(experimentlist)-1)    #search for a line that has an fmu different from that in gloss line
+                q = randint(0,len(experimentlist[p][2])-1)  #within line, pick a random translation
                 while experimentlist[p][2][q] in temptrans:
-                    print(experimentlist[p][2][q])
-                    print(temptrans)
+                    print('i found my own translation')
                     q = randint(0, len(experimentlist[p][2]) - 1)
                 other_resp = experimentlist[p][2][q]
             else:
-                while experimentlist[p][1] == FMU:
-                    p = randint(0,len(experimentlist)-1)    #See line 57
-                q = randint(0,len(experimentlist[p][2])-1)  #See line 58
+                while experimentlist[p][1] == fmu:
+                    p = randint(0,len(experimentlist)-1)    #see line 57
+                q = randint(0,len(experimentlist[p][2])-1)  #see line 58
                 while experimentlist[p][2][q] in temptrans:
                     q = randint(0, len(experimentlist[l][2]) - 1)
                 other_resp = experimentlist[p][2][q]
 
-            #Print everything into output file
+            #print everything into output file
             outputlist.append([])
-            outputlist[i].append(Gloss)
+            outputlist[i].append(gloss)
             outputlist[i].append(corr_resp)
             outputlist[i].append(other_resp)
+            outputlist[i].append(fmu)
+            outputlist[i].append('')
+            outputlist[i].append('')
 
-        create_output = outputfile("pp" + str(r) + "_Random.txt", outputlist)
-        print("I'm done with pp " + str(r))
+        output_header = "gloss,corr_resp,other_resp,fmu\n"   #header line: gloss of the sign, expected response, distractor
+        create_output = outputfile("pp" + str(r) + "_ngtrandom.txt", outputlist, output_header)
+        print("i'm done with pp " + str(r))
 
-randomiser(20,inputlist)
+
+def listmaker(number):
+    for i in range(number):
+        vidlist = readfile("pp"+str(i)+"_ngtrandom.txt")
+        outputlist = []
+        output_header = "gloss,left_resp,right_resp,correct_response,fmu,phase\n"
+
+        for j in range(len(vidlist)-1):
+            gloss = vidlist[j+1][0]
+            fmu = vidlist[j+1][3]
+            if j%2 == 0:
+                print('j+1 is '+str(j+1))
+                print('even row')
+                resp_right = vidlist[j+1][1]  #correct response
+                resp_left = vidlist[j+1][2]   #distractor
+                correct_response = "r"
+            else:
+                print('j+1 is '+str(j+1))
+                print('uneven row')
+                resp_right = vidlist[j+1][2]  #distractor
+                resp_left = vidlist[j+1][1]   #correct response
+                correct_response = "l"
+
+            outputlist.append([])
+            outputlist.append([])
+            outputlist[j].append(gloss)
+            outputlist[j].append(resp_left)
+            outputlist[j].append(resp_right)
+            outputlist[j].append(correct_response)
+            outputlist[j].append(fmu)
+            outputlist[j].append("ngt")
+
+        print(outputlist)
+
+        cleanoutputlist = [x for x in outputlist if x != []]
+
+        print(cleanoutputlist)
+        create_output = outputfile("pp" + str(i) + "_ngtsigns.txt", cleanoutputlist, output_header)
+        print("i'm done with ordered list for pp " + str(i))
+
+randomiser(20, inputlist)
+listmaker(20)
--- Randomizer_Pseudosigns.py
+++ Randomizer_Pseudosigns.py
@@ -1,69 +1,116 @@
-###Randomizer für Annika
+###randomizer für annika
 from random import shuffle, choice, random, randint
 import sys
 
-#Read input file
+#read input file
 def readfile(infilename):
     inputfile = open(infilename, 'r')
     vidlist = []
     for line in inputfile:
-        line = line.replace('\n','')    #Replace newlines in original file by commas
-        line = line.split(',')          #Split lines by comma
-        vidlist.append(line)            #Create a list with all read items
+        line = line.replace('\n','')    #replace newlines in original file by commas
+        line = line.split(',')          #split lines by comma
+        vidlist.append(line)            #create a list with all read items
     return vidlist
 
-#Create output file
-def outputfile(outfilename, newlist):
+#create output file
+def outputfile(outfilename, newlist, header):
     outfile = open(outfilename, 'w')
-    outfile.write("Gloss, corr_resp, other_resp \n")    #Header line: Gloss of the sign, expected response, distractor
-    for i in range(len(newlist)):
-        outfile.write(newlist[i][0] + "," + newlist[i][1] + ',' + newlist[i][2] +  '\n')    #Write items in output file
+    outfile.write(header)
+    for z in range(len(newlist)):
+        outfile.write(newlist[z][0] + "," + newlist[z][1] + ',' + newlist[z][2] + ',' + newlist[z][3] + ',' + newlist[z][4] + ',' + newlist[z][5] + '\n')    #write items in output file
     outfile.close()
 
-translation = []    #List of possible translations per item, to be stacked within list structure
-templist =[]        #Serves for matching FMUs with translation options
-experimentlist = [] #List with stacked list per line and within that a stacked list with the translations
-outputlist = []     #List with structure for outputfile
-resplist = readfile("NGTsignsForPseudo.txt")    #Inputfile for list of response options: GLOSS, FMU, translation1, translation2,...,translationN
-vidlist = readfile("Pseudosigns.txt")           #List of videos: VIDEO, FMU
 
-# Create list of FMUs with translation options
-for i in range(len(resplist)):
-    for j in range(len(resplist[i])):
-        if j < 2:
-            translation = []    #Separate GLOSS and FMU from translations
-        else:
-            translation.append(resplist[i][j])  #Append all translations in line
-    templist.append(resplist[i][1]) #Append FMU to templist
-    templist.append(translation)    #Append all translation options for FMU in this line
-    experimentlist.append(templist)
-    templist = []
+def randomiser(number, inputfile, respfile):
+    for y in range(number):
+        translation = []    #list of possible translations per item, to be stacked within list structure
+        templist =[]        #serves for matching fmus with translation options
+        experimentlist = [] #list with stacked list per line and within that a stacked list with the translations
+        outputlist = []     #list with structure for outputfile
+        resplist = readfile(respfile)    #inputfile for list of response options: gloss, fmu, translation1, translation2,...,translationn
+        vidlist = readfile(inputfile)           #list of videos: video, fmu
 
-print(experimentlist)
+        # create list of fmus with translation options
+        for i in range(len(resplist)):
+            for j in range(len(resplist[i])):
+                if j < 2:
+                    translation = []    #separate gloss and fmu from translations
+                else:
+                    translation.append(resplist[i][j])  #append all translations in line
+            templist.append(resplist[i][1]) #append fmu to templist
+            templist.append(translation)    #append all translation options for fmu in this line
+            experimentlist.append(templist)
+            templist = []
 
-for i in range(len(experimentlist)):
-    Gloss = vidlist[i][0]
-    FMU = vidlist[i][1]
-    
-    # Search for correct response -> shares FMU with GLOSS-line
-    l = randint(0,len(experimentlist)-1)
-    while experimentlist[l][0] != FMU:          #Search for a line that has the target FMU
-        l = randint(0,len(experimentlist)-1)
-    o = randint(0,len(experimentlist[l][1])-1)  #Within line, pick a random translation
-    corr_resp = experimentlist[l][1][o]
+        for i in range(len(experimentlist)):
+            gloss = vidlist[i][0]
+            fmu = vidlist[i][1]
 
-    #Search for distractor -> FMU different from GLOSS-line
-    p = randint(0,len(experimentlist)-1)
-    while experimentlist[p][0] == FMU:          #Search for line that has an FMU different from that in GLOSS line
-        p = randint(0,len(experimentlist)-1)
-    q = randint(0,len(experimentlist[p][1])-1)  #Within line, pick a random translation
-    other_resp = experimentlist[p][1][q]
+            # search for correct response -> shares fmu with gloss-line
+            l = randint(0,len(experimentlist)-1)
+            while experimentlist[l][0] != fmu:          #search for a line that has the target fmu
+                l = randint(0,len(experimentlist)-1)
+            o = randint(0,len(experimentlist[l][1])-1)  #within line, pick a random translation
+            corr_resp = experimentlist[l][1][o]
 
-    #Print everything into output file
-    outputlist.append([])
-    outputlist[i].append(Gloss)
-    outputlist[i].append(corr_resp)
-    outputlist[i].append(other_resp)
+            #search for distractor -> fmu different from gloss-line
+            p = randint(0,len(experimentlist)-1)
+            while experimentlist[p][0] == fmu:          #search for line that has an fmu different from that in gloss line
+                p = randint(0,len(experimentlist)-1)
+            q = randint(0,len(experimentlist[p][1])-1)  #within line, pick a random translation
+            other_resp = experimentlist[p][1][q]
 
-create_output = outputfile("Output.txt", outputlist)
+            #print everything into output file
+            outputlist.append([])
+            outputlist[i].append(gloss)
+            outputlist[i].append(corr_resp)
+            outputlist[i].append(other_resp)
+            outputlist[i].append(fmu)
+            outputlist[i].append('')
+            outputlist[i].append('')
 
+        output_header = "gloss,correct response,other response,fmu"
+        create_output = outputfile("pp" + str(y) +"_randompseudosigns.txt", outputlist, output_header)
+
+def listmaker(number):
+    for i in range(number):
+        vidlist = readfile("pp"+str(i)+"_randompseudosigns.txt")
+        outputlist = []
+        output_header = "gloss,left_resp,right_resp,correct_response,fmu,phase\n"
+
+        for j in range(len(vidlist)-1):
+            gloss = vidlist[j+1][0]
+            fmu = vidlist[j+1][3]
+            if j%2 == 0:
+                print('j+1 is '+str(j+1))
+                print('even row')
+                resp_right = vidlist[j+1][1]  #correct response
+                resp_left = vidlist[j+1][2]   #distractor
+                correct_response = "r"
+            else:
+                print('j+1 is '+str(j+1))
+                print('uneven row')
+                resp_right = vidlist[j+1][2]  #distractor
+                resp_left = vidlist[j+1][1]   #correct response
+                correct_response = "l"
+
+            outputlist.append([])
+            outputlist.append([])
+            outputlist[j].append(gloss)
+            outputlist[j].append(resp_left)
+            outputlist[j].append(resp_right)
+            outputlist[j].append(correct_response)
+            outputlist[j].append(fmu)
+            outputlist[j].append("pseudo")
+
+        print(outputlist)
+
+        cleanoutputlist = [x for x in outputlist if x != []]
+
+        print(cleanoutputlist)
+        create_output = outputfile("pp" + str(i) + "_pseudoigns.txt", cleanoutputlist, output_header)
+        print("i'm done with ordered list for pp " + str(i))
+
+
+randomiser(20, "pseudosigns.txt", "ngtsignsforpseudo.txt")
+listmaker(20)
